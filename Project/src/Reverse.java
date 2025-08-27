08b7e5f6c1ff4642398da643e54b5d2f1895308c
Deepika <suyaldeepika27@gmail.com>
2025-08-27 13:13:14 +0530

    reverse a string

--- Project/src/Reverse.java
+++ Project/src/Reverse.java
@@ -1,15 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Reverse {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
+            String originalString = "Hello, Java!";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // Create a StringBuilder object with the original string
+            StringBuilder stringBuilder = new StringBuilder(originalString);
+
+            // Use the reverse() method to reverse the characters
+            stringBuilder.reverse();
+
+            // Convert the reversed StringBuilder back to a String
+            String reversedString = stringBuilder.toString();
+
+            System.out.println("Original String: " + originalString);
+            System.out.println("Reversed String: " + reversedString);
         }
     }
-}
+
